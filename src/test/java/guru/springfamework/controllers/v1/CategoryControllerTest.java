3f999b0b0d73e4920b17ef00184ec5095047b310
Iiro Kokkonen <iiro.kokkonen@cinia.fi>
2021-01-28 13:59:54 +0200

    Category controller.

---
+++ src/test/java/guru/springfamework/controllers/v1/CategoryControllerTest.java
@@ -0,0 +1,75 @@
+package guru.springfamework.controllers.v1;
+
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.services.CategoryService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class CategoryControllerTest {
+
+    @Mock
+    CategoryService categoryService;
+
+    @InjectMocks
+    CategoryController categoryController;
+
+    MockMvc mockMvc;
+
+    List<CategoryDTO> categoryDTOS;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(categoryController).build();
+
+        categoryDTOS = new ArrayList<>();
+        CategoryDTO c1 = new CategoryDTO();
+        c1.setId(1L);
+        c1.setName("test1");
+        categoryDTOS.add(c1);
+        CategoryDTO c2 = new CategoryDTO();
+        c2.setId(2L);
+        c2.setName("test2");
+        categoryDTOS.add(c2);
+    }
+
+
+    @Test
+    public void testListCategories() throws Exception {
+        when(categoryService.getAllCategories()).thenReturn(categoryDTOS);
+
+        mockMvc.perform(get("/api/v1/categories/").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+        .andExpect(jsonPath("$.categories", hasSize(2)));
+    }
+
+    @Test
+    public void testGetByNameCategories() throws Exception {
+        when(categoryService.getCategoriesByName(anyString())).thenReturn(categoryDTOS.get(0));
+
+        mockMvc.perform(get("/api/v1/categories/test1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo("test1")));
+    }
+}
