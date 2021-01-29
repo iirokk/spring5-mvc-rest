3251add7884e7a26cd99a5340dba03bc930c3a8a
Iiro Kokkonen <iiro.kokkonen@cinia.fi>
2021-01-29 12:20:52 +0200

    Test WebMvc refactor.

--- src/test/java/guru/springfamework/controllers/v1/VendorControllerTest.java
+++ src/test/java/guru/springfamework/controllers/v1/VendorControllerTest.java
@@ -6,10 +6,15 @@ import guru.springfamework.services.ResourceNotFoundException;
 import guru.springfamework.services.VendorService;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -26,22 +31,15 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = {VendorController.class})
 public class VendorControllerTest extends AbstractRestControllerTest {
 
-    @Mock
+    @MockBean
     VendorService vendorService;
 
-    @InjectMocks
-    VendorController vendorController;
-
+    @Autowired
     MockMvc mockMvc;
-
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-
-        mockMvc = MockMvcBuilders.standaloneSetup(vendorController).setControllerAdvice(RestResponseEntityExceptionHandler.class).build();
-    }
 
     @Test
     public void testListVendors() throws Exception {
